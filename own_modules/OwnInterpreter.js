dabdb847ea776f4c42b6e82e959a72ca9e61cc3b
josehr1108 <josehr1108@gmail.com>
2017-11-05 03:38:27 -0600

    firstDesignator

--- own_modules/OwnInterpreter.js
+++ own_modules/OwnInterpreter.js
@@ -16,45 +16,35 @@ function OwnInterpreter (methodToExecute,loadDataWarehouse){
     warehouse = loadDataWarehouse;
     method = methodToExecute;
     parseArray(method.parameters);
-    console.log("Array parseado");
-    console.log(method.parameters);
     return this;
 }
-
 OwnInterpreter.prototype = Object.create(parserVisitor.prototype);
 OwnInterpreter.prototype.constructor = OwnInterpreter;
 
-
 OwnInterpreter.prototype.visitProgram = function(ctx) {
-
     let warehouseMethod = warehouse.searchElement(warehouse, method.name);
     let newCtx = warehouseMethod.data.decl;
     newCtx.methodToExecute = warehouseMethod.data;
-
 
     this.visit(newCtx);
 };
 
 OwnInterpreter.prototype.visitConstDecl = function(ctx) {
-
     return
 };
 
 OwnInterpreter.prototype.visitVarDecl = function(ctx) {
-
     return
 };
 
 OwnInterpreter.prototype.visitClassDecl = function(ctx) {
-
-
     return
 };
 
 OwnInterpreter.prototype.visitMethodDecl = function(ctx) {
 
     let pilaExpr = [];
-    let parameters = warehouseMethod.data.parameters;
+    let parameters = ctx.methodToExecute.parameters;
    
     for (let i = 0; i < parameters.length; i++){
         if(parameters[i].reference == "formPars"){
@@ -67,7 +57,7 @@ OwnInterpreter.prototype.visitMethodDecl = function(ctx) {
         }
 
         if(parameters[i].reference == "varDecl"){
-            localVar = {}
+            localVar = {};
             localVar.name = parameters[i].name;
             localParameter.value = undefined;
             localParameter.type = parameters[i].type;
@@ -78,7 +68,7 @@ OwnInterpreter.prototype.visitMethodDecl = function(ctx) {
     let newCtx = ctx.block();
     newCtx.pilaExpr = pilaExpr;
     this.visit(newCtx);
-    return
+    return;
   
 };
 
@@ -87,8 +77,7 @@ OwnInterpreter.prototype.visitFormPars = function(ctx) {
     
     return 
 };
-
-/*-------------------------------Types ---------------------------------------------------*/
+/*-------------------------------       Types      ---------------------------------------------------*/
 OwnInterpreter.prototype.visitIdentType = function(ctx) {
     return {type : 1, identifier : ctx.IDENT().getSymbol().text};
 };
@@ -116,8 +105,10 @@ OwnInterpreter.prototype.visitStringType = function(ctx) {
 /* ------------------------------------------- Statement ----------------------------------------------------------*/
 
 OwnInterpreter.prototype.visitFirstDesignStatement = function(ctx) {
-    let identifier = this.visit(ctx.designator());
-    //ctx.pilaExpr
+
+    let identifier = ctx.designator();
+    identifier.pilaExpr = ctx.pilaExpr;
+    let response = this.visit(identifier);
 
     let expression = ctx.expr();
     let actPars = ctx.actPars();
@@ -127,6 +118,9 @@ OwnInterpreter.prototype.visitFirstDesignStatement = function(ctx) {
 
     if(expression){
         let asignExpression = this.visit(expression);
+    }
+    else if(leftPar.length){//llamada a metodo
+
     }
 };
 
@@ -169,9 +163,6 @@ OwnInterpreter.prototype.visitWhileStatement = function(ctx) {
 };
 
 OwnInterpreter.prototype.visitForeachStatement = function(ctx) {
-
-
-
   return
 };
 
@@ -185,25 +176,16 @@ OwnInterpreter.prototype.visitReturnStatement = function(ctx) {
 };
 
 OwnInterpreter.prototype.visitReadStatement = function(ctx) {
-   
-
-   
     this.visit(ctx.designator());
-    return 
-
-    
+    return
 };
 
 OwnInterpreter.prototype.visitWriteStatement = function(ctx) {
-
     return
 };
 
 OwnInterpreter.prototype.visitBlockStatement = function(ctx) {
-    
-
     return this.visit(ctx.block());
-   
 };
 
 OwnInterpreter.prototype.visitSemicolonStatement = function(ctx) {
@@ -213,7 +195,6 @@ OwnInterpreter.prototype.visitSemicolonStatement = function(ctx) {
 /*************************************************************************************************************/
 
 OwnInterpreter.prototype.visitBlock = function(ctx) {
-
     let statements = ctx.statement();
     if(statements){
         for (let i=0; i <= statements.length-1; i++) {
@@ -222,17 +203,15 @@ OwnInterpreter.prototype.visitBlock = function(ctx) {
             this.visit(statement);
         }
     }
-    return
+    return;
 };
 
 OwnInterpreter.prototype.visitActPars = function(ctx) {
-   
     let typeActPars = this.visit(ctx.expr(0));
     
      for (let i=1; i <= ctx.expr().length - 1; i++) {
          if(typeActPars.typeTerminal === 3 || typeActPars.typeTerminal === 4){
             typeActPars = this.visit(ctx.factor(i));
- 
          }
          else{
              // retorna 22 si se trato de multiplicar tipos diferentes
@@ -377,27 +356,21 @@ OwnInterpreter.prototype.visitExpressionFactor = function(ctx) {
 
 /*------------------------------------------------------------------------------------------------------------------------------*/
 OwnInterpreter.prototype.visitDesignator = function(ctx) {
-    return ctx.IDENT();
-/*
+    let ident1 = ctx.IDENT(0).getSymbol().text;
+    let returnData = {variableName: ident1};
+
     let ident2 = ctx.IDENT(1);
-    let leftBracketAmount = ctx.LEFT_SQUARE_BRACKET().length;
-    if(ident2){ //hay 2 o mas identifiers, se va por el AT IDENT
-        let identLenght = ctx.IDENT().length-1;
-        for(let i=1;i <= ctx.IDENT().length-1;i++){
-            
-        }
+    let expr = ctx.expr();
+
+    if(ident2){
+        returnData.propertyName = ident2.getSymbol().text;
     }
-    else if(leftBracketAmount != 0){
-        for(let i = 0; i <= leftBracketAmount - 1; i++){
-            i
-
-            
-            this.visit(ctx.expr(i));
-        
-        }
-    }*/
-
-
+    else if(expr.length){
+        expr.pilaExpr = ctx.pilaExpr;
+        let response = this.visit(expr);
+        returnData.arrayPosition = response; ///suponiendo response es tipo entero
+    }
+    return returnData
 };
 /*----------------------------------------------- Operadores Logicos y Matematicos ---------------------------------------------*/
 
